65ace8080d3d9c0bbe21582c166ce5ebbb961d5d
MatthewWhitaker <matthew.w3000@gmail.com>
2019-02-05 14:34:07 -0700

    Start adding more tests

--- test/html_parser_test.dart
+++ test/html_parser_test.dart
@@ -1,91 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 void main() {
-  // test('Checks that `parse` does not throw an exception', () {
-  //   final elementList = HtmlOldParser(width: 42.0).parse("<b>Bold Text</b>");
-  //   expect(elementList, isNotNull);
-  // });
 
-  testWidgets('Tests some plain old text', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
+  testWidgets("Check that default parser does not fail on empty data", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
         home: Scaffold(
-      body: Html(data: "This is some plain text"),
-    )));
-
-    expect(find.text("This is some plain text"), findsOneWidget);
-  });
-
-  testWidgets('Tests that a <b> element gets rendered correctly',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Html(
-          data: "<b>Bold Text</b>",
+          body: Html(
+            data: "",
+          ),
         ),
       ),
-    ));
-
-    expect(find.byType(RichText), findsOneWidget);
+    );
   });
 
-  testWidgets(
-      'Tests that a combination of elements and text nodes gets rendered',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Html(
-          data: "<b>Bold Text</b> and plain text",
+  testWidgets("Check that RichText parser does not fail on empty data", (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Html(
+            data: "",
+            useRichText: true,
+          ),
         ),
       ),
-    ));
-
-    expect(find.byType(RichText), findsNWidgets(2));
-    expect(find.text(" and plain text"), findsOneWidget);
+    );
   });
 
-  testWidgets('Tests that a <i> element gets rendered correctly',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Html(
-          data: "<b>Bold Text</b>, <i>Italic Text</i> and plain text",
+  testWidgets("Check that `a` tag is rendered by both parsers", (tester) async {
+    String html = "<a href='https://github.com'>Test link</a>";
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Html(
+            data: html,
+          ),
         ),
       ),
-    ));
+    );
 
-    expect(find.byType(RichText), findsNWidgets(4));
-    expect(find.text(", "), findsOneWidget);
-    expect(find.text(" and plain text"), findsOneWidget);
-  });
-
-  testWidgets('Tests that nested elements get rendered correctly',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Html(
-          data:
-              "<b>Bold Text and <i>Italic bold text and <u>Underlined italic bold text</u></i></b>",
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Html(
+            data: html,
+            useRichText: true,
+          ),
         ),
       ),
-    ));
-
-    expect(find.byType(RichText), findsNWidgets(3));
+    );
   });
 
-  testWidgets('Tests that the header elements (h1-h6) get rendered correctly',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Html(
-          data:
-              "<h1>H1</h1><h2>H2</h2><h3>H3</h3><h4>H4</h4><h5>H5</h5><h6>H6</h6>",
+  testWidgets("Check that tapping on the `a` tag calls the callback", (tester) async {
+    String html = "<a href='https://github.com'>Test link</a>";
+    String urlTapped;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Html(
+            data: html,
+            onLinkTap: (url) {
+              urlTapped = url;
+            },
+          ),
         ),
       ),
-    ));
-
-    expect(find.byType(RichText), findsNWidgets(6));
+    );
+    await tester.tap(find.text("Test link"));
+    expect(urlTapped, "https://github.com");
   });
+
 }
