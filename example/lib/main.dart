5bc1f75e0b207bb06f23ee8e3059db89518ea9c2
Matthew Whitaker <matthew.w3000@gmail.com>
2019-09-20 08:24:00 -0600

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_html/src/block_element.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_html/style.dart';
 
