a3f2275af5ef97379b3259b0b7b84e972cc1b245
jesktop <frayay@gmail.com>
2020-03-05 02:08:11 +0800

    rt and ruby distance

--- lib/src/replaced_element.dart
+++ lib/src/replaced_element.dart
@@ -311,7 +311,7 @@ class RubyElement extends ReplacedElement {
     List<Widget> widgets = List<Widget>();
     //TODO calculate based off of parent font size.
     final rubySize = max(9.0, context.style.fontSize.size / 2);
-    final rubyYPos = rubySize + 2;
+    final rubyYPos = rubySize + rubySize / 2;
     element.nodes.forEach((c) {
       if (c.nodeType == dom.Node.TEXT_NODE) {
         textNode = c;
