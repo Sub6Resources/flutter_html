4260ef7b312b60c16eb17291d5bccff92f29bda1
Sorunome <mail@sorunome.de>
2020-05-07 16:38:12 +0200

    fix up stuffs

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_matrix_html/flutter_html.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'test_data.dart';
--- test/html_parser_test.dart
+++ test/html_parser_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/rich_text_parser.dart';
+import 'package:flutter_matrix_html/rich_text_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_matrix_html/flutter_html.dart';
 
 void main() {
   testWidgets("Check that default parser does not fail on empty data",
