48a65f0105879a1c4bcb85946574298a51ecb6f8
tanay <tanay@neotia.in>
2020-12-20 11:36:34 -0500

    Fix alt text not showing up on broken images

--- lib/src/replaced_element.dart
+++ lib/src/replaced_element.dart
@@ -129,14 +129,23 @@ class ImageContentElement extends ReplacedElement {
         },
       );
       Completer<Size> completer = Completer();
-      Image image = Image.network(src);
+      Image image = Image.network(src, frameBuilder: (ctx, child, frame, _) {
+        if (frame == null) {
+          completer.completeError("error");
+          return child;
+        } else {
+          return child;
+        }
+      });
       image.image.resolve(ImageConfiguration()).addListener(
         ImageStreamListener(
               (ImageInfo image, bool synchronousCall) {
             var myImage = image.image;
             Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
             completer.complete(size);
-          },
+          }, onError: (object, stacktrace) {
+            completer.completeError(object);
+          }
         ),
       );
       imageWidget = FutureBuilder<Size>(
