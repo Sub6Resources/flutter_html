33a29419f16cd26211b9604a6f02922213b72621
tanay <tanay@neotia.in>
2021-02-13 15:32:35 -0500

    Revert "Remove unnecessary code"
    This reverts commit 29668fbd6c8c8f051618f469f26306812728f36b.
    
    ng

--- lib/src/replaced_element.dart
+++ lib/src/replaced_element.dart
@@ -120,7 +120,7 @@ class IframeContentElement extends ReplacedElement {
             : JavascriptMode.disabled,
         navigationDelegate: navigationDelegate,
         gestureRecognizers: {
-          Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer()),
+          Factory(() => PlatformViewVerticalGestureRecognizer())
         },
       ),
     );
@@ -371,4 +371,43 @@ ReplacedElement parseReplacedElement(
     default:
       return EmptyContentElement(name: element.localName);
   }
+}
+
+// TODO(Sub6Resources): Remove when https://github.com/flutter/flutter/issues/36304 is resolved
+class PlatformViewVerticalGestureRecognizer
+    extends VerticalDragGestureRecognizer {
+  PlatformViewVerticalGestureRecognizer({PointerDeviceKind kind})
+      : super(kind: kind);
+
+  Offset _dragDistance = Offset.zero;
+
+  @override
+  void addPointer(PointerEvent event) {
+    startTrackingPointer(event.pointer);
+  }
+
+  @override
+  void handleEvent(PointerEvent event) {
+    _dragDistance = _dragDistance + event.delta;
+    if (event is PointerMoveEvent) {
+      final double dy = _dragDistance.dy.abs();
+      final double dx = _dragDistance.dx.abs();
+
+      if (dy > dx && dy > kTouchSlop) {
+        // vertical drag - accept
+        resolve(GestureDisposition.accepted);
+        _dragDistance = Offset.zero;
+      } else if (dx > kTouchSlop && dx > dy) {
+        // horizontal drag - stop tracking
+        stopTrackingPointer(event.pointer);
+        _dragDistance = Offset.zero;
+      }
+    }
+  }
+
+  @override
+  String get debugDescription => 'horizontal drag (platform view)';
+
+  @override
+  void didStopTrackingLastPointer(int pointer) {}
 }
