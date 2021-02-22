a48fd9d71304572d099fa200183627d4b46e9e6b
Eric Kok <erickok@gmail.com>
2021-02-23 00:06:02 +0100

    Adds support for data image uri with encoded svg
    This supports SVG as base64 encoded data blob or inline tags.
    
    This is an alternative to #550 which did not use the new image API at all.

--- lib/image_render.dart
+++ lib/image_render.dart
@@ -11,10 +11,15 @@ typedef ImageSourceMatcher = bool Function(
   dom.Element element,
 );
 
-ImageSourceMatcher base64DataUriMatcher() => (attributes, element) =>
-    _src(attributes) != null &&
-    _src(attributes).startsWith("data:image") &&
-    _src(attributes).contains("base64,");
+final _dataUriFormat = RegExp("^(?<scheme>data):(?<mime>image\/[\\w\+\-\.]+)(?<encoding>;base64)?\,(?<data>.*)");
+
+ImageSourceMatcher dataUriMatcher({String encoding = 'base64', String mime}) => (attributes, element) {
+      if (_src(attributes) == null) return false;
+      final dataUri = _dataUriFormat.firstMatch(_src(attributes));
+      return dataUri != null &&
+          (mime == null || dataUri.namedGroup('mime') == mime) &&
+          (encoding == null || dataUri.namedGroup('encoding') == ';$encoding');
+    };
 
 ImageSourceMatcher networkSourceMatcher({
   List<String> schemas: const ["https", "http"],
@@ -33,8 +38,8 @@ ImageSourceMatcher networkSourceMatcher({
       }
     };
 
-ImageSourceMatcher assetUriMatcher() => (attributes, element) =>
-    _src(attributes) != null && _src(attributes).startsWith("asset:");
+ImageSourceMatcher assetUriMatcher() =>
+    (attributes, element) => _src(attributes) != null && _src(attributes).startsWith("asset:");
 
 typedef ImageRender = Widget Function(
   RenderContext context,
@@ -43,8 +48,7 @@ typedef ImageRender = Widget Function(
 );
 
 ImageRender base64ImageRender() => (context, attributes, element) {
-      final decodedImage =
-          base64.decode(_src(attributes).split("base64,")[1].trim());
+      final decodedImage = base64.decode(_src(attributes).split("base64,")[1].trim());
       precacheImage(
         MemoryImage(decodedImage),
         context.buildContext,
@@ -56,8 +60,7 @@ ImageRender base64ImageRender() => (context, attributes, element) {
         decodedImage,
         frameBuilder: (ctx, child, frame, _) {
           if (frame == null) {
-            return Text(_alt(attributes) ?? "",
-                style: context.style.generateTextStyle());
+            return Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
           }
           return child;
         },
@@ -79,8 +82,7 @@ ImageRender assetImageRender({
           height: height ?? _height(attributes),
           frameBuilder: (ctx, child, frame, _) {
             if (frame == null) {
-              return Text(_alt(attributes) ?? "",
-                  style: context.style.generateTextStyle());
+              return Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
             }
             return child;
           },
@@ -109,8 +111,7 @@ ImageRender networkImageRender({
         },
       );
       Completer<Size> completer = Completer();
-      Image image =
-          Image.network(src, frameBuilder: (ctx, child, frame, _) {
+      Image image = Image.network(src, frameBuilder: (ctx, child, frame, _) {
         if (frame == null) {
           if (!completer.isCompleted) {
             completer.completeError("error");
@@ -124,8 +125,7 @@ ImageRender networkImageRender({
       image.image.resolve(ImageConfiguration()).addListener(
             ImageStreamListener((ImageInfo image, bool synchronousCall) {
               var myImage = image.image;
-              Size size =
-                  Size(myImage.width.toDouble(), myImage.height.toDouble());
+              Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
               if (!completer.isCompleted) {
                 completer.complete(size);
               }
@@ -147,15 +147,14 @@ ImageRender networkImageRender({
               frameBuilder: (ctx, child, frame, _) {
                 if (frame == null) {
                   return altWidget?.call(_alt(attributes)) ??
-                      Text(_alt(attributes) ?? "",
-                          style: context.style.generateTextStyle());
+                      Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
                 }
                 return child;
               },
             );
           } else if (snapshot.hasError) {
-            return altWidget?.call(_alt(attributes)) ?? Text(_alt(attributes) ?? "",
-                style: context.style.generateTextStyle());
+            return altWidget?.call(_alt(attributes)) ??
+                Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
           } else {
             return loadingWidget?.call() ?? const CircularProgressIndicator();
           }
@@ -163,12 +162,31 @@ ImageRender networkImageRender({
       );
     };
 
+ImageRender svgDataImageRender() => (context, attributes, element) {
+      final dataUri = _dataUriFormat.firstMatch(_src(attributes));
+      final data = dataUri.namedGroup('data');
+      if (dataUri.namedGroup('encoding') == ';base64') {
+        final decodedImage = base64.decode(data.trim());
+        return SvgPicture.memory(
+          decodedImage,
+          width: _width(attributes),
+          height: _height(attributes),
+        );
+      }
+      return SvgPicture.string(Uri.decodeFull(data));
+    };
+
 ImageRender svgNetworkImageRender() => (context, attributes, element) {
-      return SvgPicture.network(attributes["src"]);
+      return SvgPicture.network(
+        attributes["src"],
+        width: _width(attributes),
+        height: _height(attributes),
+      );
     };
 
 final Map<ImageSourceMatcher, ImageRender> defaultImageRenders = {
-  base64DataUriMatcher(): base64ImageRender(),
+  dataUriMatcher(mime: 'image/svg+xml', encoding: null): svgDataImageRender(),
+  dataUriMatcher(): base64ImageRender(),
   assetUriMatcher(): assetImageRender(),
   networkSourceMatcher(extension: "svg"): svgNetworkImageRender(),
   networkSourceMatcher(): networkImageRender(),
--- test/image_render_source_matcher_test.dart
+++ test/image_render_source_matcher_test.dart
@@ -79,8 +79,8 @@ void main() {
       expect(_match(matcher, ''), isFalse);
     });
   });
-  group("base64 image data uri matcher", () {
-    ImageSourceMatcher matcher = base64DataUriMatcher();
+  group("default (base64) image data uri matcher", () {
+    ImageSourceMatcher matcher = dataUriMatcher();
     test("matches a full png base64 data uri", () {
       expect(
           _match(matcher,
@@ -103,6 +103,43 @@ void main() {
           isFalse);
     });
     test("doesn't match base64 non-image data uri", () {
+      expect(_match(matcher, 'data:text/plain;base64,'), isFalse);
+    });
+    test("doesn't non-data schema", () {
+      expect(_match(matcher, 'http:'), isFalse);
+    });
+    test("doesn't match null", () {
+      expect(_match(matcher, null), isFalse);
+    });
+    test("doesn't match empty", () {
+      expect(_match(matcher, ''), isFalse);
+    });
+  });
+  group("custom image data uri matcher", () {
+    ImageSourceMatcher matcher =
+        dataUriMatcher(encoding: null, mime: 'image/svg+xml');
+    test("matches an svg data uri with base64 encoding", () {
+      expect(
+          _match(matcher,
+              'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB2aWV3Qm94PSIwIDAgMzAgMjAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxjaXJjbGUgY3g9IjE1IiBjeT0iMTAiIHI9IjEwIiBmaWxsPSJncmVlbiIvPgo8L3N2Zz4='),
+          isTrue);
+    });
+    test("matches an svg data uri without specified encoding", () {
+      expect(
+          _match(matcher,
+              'data:image/svg+xml,%3C?xml version="1.0" encoding="UTF-8"?%3E%3Csvg viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg"%3E%3Ccircle cx="15" cy="10" r="10" fill="green"/%3E%3C/svg%3E'),
+          isTrue);
+    });
+    test("matches base64 data uri without data", () {
+      expect(_match(matcher, 'data:image/svg+xml;base64,'), isTrue);
+    });
+    test("doesn't match non-base64 image data uri", () {
+      expect(
+          _match(matcher,
+              'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='),
+          isFalse);
+    });
+    test("doesn't match different mime data uri", () {
       expect(_match(matcher, 'data:text/plain;base64,'), isFalse);
     });
     test("doesn't non-data schema", () {
