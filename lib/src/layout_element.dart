9d7890216ba8417d841da4ab438c7d47c5b080c5
windows7lake <979506634@qq.com>
2021-09-24 08:32:58 +0800

    Update layout_element.dart
    remove unrelative code

--- lib/src/layout_element.dart
+++ lib/src/layout_element.dart
@@ -112,9 +112,6 @@ class TableLayoutElement extends LayoutElement {
           columni++;
         }
         if (child is TableCellElement) {
-          String width = child.attributes["width"] ?? "";
-          width.replaceAll("px", "");
-          final pxSize = double.tryParse(width) ?? 120;
           cells.add(GridPlacement(
             child: Container(
               width: double.infinity,
