283c3afa8222687e0cdc3c1657be36c68e8dd944
Eric Kok <erickok@gmail.com>
2022-04-14 13:51:00 +0200

    Don't crash when video ir iframe uses unsupported height/width; fixes #1033

--- packages/flutter_html_iframe/lib/iframe_mobile.dart
+++ packages/flutter_html_iframe/lib/iframe_mobile.dart
@@ -7,11 +7,11 @@ import 'package:webview_flutter/webview_flutter.dart';
 CustomRender iframeRender({NavigationDelegate? navigationDelegate}) => CustomRender.widget(widget: (context, buildChildren) {
   final sandboxMode = context.tree.element?.attributes["sandbox"];
   final UniqueKey key = UniqueKey();
+  final givenWidth = double.tryParse(context.tree.element?.attributes['width'] ?? "");
+  final givenHeight = double.tryParse(context.tree.element?.attributes['height'] ?? "");
   return Container(
-    width: double.tryParse(context.tree.element?.attributes['width'] ?? "")
-        ?? (double.tryParse(context.tree.element?.attributes['height'] ?? "") ?? 150) * 2,
-    height: double.tryParse(context.tree.element?.attributes['height'] ?? "")
-        ?? (double.tryParse(context.tree.element?.attributes['width'] ?? "") ?? 300) / 2,
+    width: givenWidth ?? (givenHeight ?? 150) * 2,
+    height: givenHeight ?? (givenWidth ?? 300) / 2,
     child: ContainerSpan(
       style: context.style,
       newContext: context,
